48f79c4d164f5a70b216ff442fa48a126cea0387
SerhiiKutovyi <109694632+SerhiiKutovyi@users.noreply.github.com>
2023-11-27 17:22:13 +0200

    7kyu

--- src/codewars.js
+++ src/codewars.js
@@ -13,13 +13,51 @@
 
 // console.log(countSheeps(sheep));
 
-function makeNegative(num) {
-//   if (num < 0) {
-//     return num
+// function makeNegative(num) {
+// //   if (num < 0) {
+// //     return num
+// //   }
+//   //  return num * -1
+
+//   return -Math.abs(num)
+// }
+
+// console.log(makeNegative(10));
+
+// const quarterOf = month => {
+//   let cost;
+//   switch (month) {
+//     case '1,2,3':
+//       cost = 1;
+//       break;
+
+//     case 4 | 5 | 6:
+//       cost = 2;
+//       break;
+//     case 7 | 8 | 9:
+//       cost = 3;
+//       break;
+//     case 10 | 11 | 12:
+//       cost = 4;
+//       break;
+
+//     default:
+//       console.log('Invalid');
 //   }
-  //  return num * -1
+//   return cost
+// };
 
-  return -Math.abs(num)
-}
+// console.log(quarterOf(8));
+const quarterOf = month => {
+  if ( month <= 3) {
+    return 1;
+  } else if ( month <= 6) {
+    return 2;
+  } else if ( month <= 9) {
+    return 3;
+  } else if (month <= 12) {
+    return 4;
+  }
+};
 
-console.log(makeNegative(10));
+console.log(quarterOf(3));
