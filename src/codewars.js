64769f58e84cfa1d69a7ae98761ec5f8bdc92878
SerhiiKutovyi <109694632+SerhiiKutovyi@users.noreply.github.com>
2023-11-27 15:14:46 +0200

    sheep

--- src/codewars.js
+++ src/codewars.js
@@ -7,8 +7,19 @@
 
 // console.log(countSheeps(sheep));
 
-const sheep = [undefined, null, false, true];
+// const sheep = [undefined, null, false, true];
 
-const countSheeps = sheep => sheep.filter(Boolean).length;
+// const countSheeps = sheep => sheep.filter(Boolean).length;
 
-console.log(countSheeps(sheep));
+// console.log(countSheeps(sheep));
+
+function makeNegative(num) {
+//   if (num < 0) {
+//     return num
+//   }
+  //  return num * -1
+
+  return -Math.abs(num)
+}
+
+console.log(makeNegative(10));
